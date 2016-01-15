af47dd4242b5c9cf8bbda88c09183aa9895bb8d5
agenau <m1557856@compass28.(none)>
2016-01-15 13:59:23 +0100

    Additional accessors, compiling version

--- Color.h
+++ Color.h
@@ -1,21 +1,22 @@
 
-namespace raytracing;
+namespace raytracing {
 
-class Color {
-private:
-  double r, g, b;
-public:
-  Color(double r=0, double g=0, double b=0){
-    SetR(r); SetG(g); SetB(b);
-  }
-  double R(){ return r; }
-  double G(){ return g; }
-  double B(){ return b; }
-  void SetR(double h){ r = (h>1?1:(h<0?0:h)); }
-  void SetG(double h){ g = (h>1?1:(h<0?0:h)); }
-  void SetB(double h){ b = (h>1?1:(h<0?0:h)); }
-  unsigned char R(){ return r; }
-  unsigned char G(){ return g; }
-  unsigned char B(){ return b; }
+  class Color {
+  private:
+    double r, g, b;
+  public:
+    Color(double r=0, double g=0, double b=0){
+      SetR(r); SetG(g); SetB(b);
+    }
+    double R(){ return r; }
+    double G(){ return g; }
+    double B(){ return b; }
+    void SetR(double h){ r = (h>1?1:(h<0?0:h)); }
+    void SetG(double h){ g = (h>1?1:(h<0?0:h)); }
+    void SetB(double h){ b = (h>1?1:(h<0?0:h)); }
+    unsigned char getCharR(){ return r*255; }
+    unsigned char getCharG(){ return g*255; }
+    unsigned char getCharB(){ return b*255; }
+  };
 
 }
