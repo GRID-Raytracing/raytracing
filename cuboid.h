1c1b06b65927aae282e62451008da405b3bf6662
Anna <annaclevenhaus@hotmail.de>
2016-01-15 13:35:43 +0100

    Change

--- cuboid.h
+++ cuboid.h
@@ -1,16 +1,22 @@
+#ifndef CUBIOD_H
+#define CUBIOD_H
+
 namespace raytracing{
-class cuboid: public drawableObject
+class Cuboid: public drawableObject
 {
 private:
 	double xSize;
 	double ySize;
 	double zSize;
 public:
-	void setxSize(double x):xSize(x){};
-	void setySize(double y):ySize(y){};
-	void setzSize(double z):zSize(z){};
+	Cuboid(){}
+	Cuboid(double ref, Color col, double t, double x, double y, double z): drawableObject(rev,col,t),xSize(x), ySize(y), zSize(z) {}	
+//	void setxSize(double x):xSize(x){};
+//	void setySize(double y):ySize(y){};
+//	void setzSize(double z):zSize(z){};
 
 	Vector3D intersection (Ray r);
 	Color getColorAtIntersection( Vector3D intersection, Ray r);
 };
 }
+#endif // CUBIOD_H
