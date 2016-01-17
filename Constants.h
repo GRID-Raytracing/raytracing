b1778bd498d8ab9c036a54829a8a42876afe0cfa
Jens Jäschke <jensm1@bbjh.de>
2016-01-17 15:22:14 +0100

    Implemented Observer::render()

---
+++ Constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+//this file contains global configuration constants 
+
+#define MAX_RECURSION 1
+
+
+
+#endif //Constants_H
--- Observer.cpp
+++ Observer.cpp
@@ -1,8 +1,10 @@
 #include "Observer.h"
+#include "Ray.h"
+#include "Constants.h"
 namespace raytracing {
     
     Observer::Observer(Vector3D pos, Vector3D dir, double dist, Vector3D xDir, Vector3D yDir, unsigned int xRes, unsigned int yRes):
-            Object(pos), viewingDirection(dir), displayDistance(dist), xDirection(xDir), yDirection(yDir), xResolution(xRes), yResolution(yRes) {
+            Object(pos), viewingDirection(dir.normalise()), displayDistance(dist), xDirection(xDir), yDirection(yDir), xResolution(xRes), yResolution(yRes) {
                 image.resize(xResolution);
                 for(unsigned int i = 0; i<xResolution; i++) {
                     image[i].resize(yResolution);
@@ -24,7 +26,7 @@ namespace raytracing {
             for(unsigned int i = 0; i<xResolution; i++){
                 for(unsigned int j = 0; j<yResolution; j++){
                     Color& c = image[i][j];
-                    bitmap.set_pixel(i,j,c.R(),c.G(),c.B());
+                    bitmap.set_pixel(i,j,c.getCharR(),c.getCharG(),c.getCharB());
                 }
             }
             bitmap.save_image(path);
@@ -33,6 +35,23 @@ namespace raytracing {
         
         cerr << "Error in Observer::exportImage(): Format not supported!" << endl;
         return;
+    }
+    
+    void Observer::render() {
+        //get position of top left image corner relative to observer position
+        Vector3D topLeft = displayDistance*viewingDirection + (-0.5)*xRessolution*xDirection + (-0.5)*yResolution*yDirection;
+        //center of top left pixel
+        topLeft = topLeft + 0.5*xDirection + 0.5*yDirection;
+        
+        //trace rays for each pixel
+        for(unsigned int i=0; i<xResolution; i++) {
+            for(unsigned int j=0; j<yResolution; j++) {
+                Vector3D direction = topLeft + i*xDirection + j*yDirection;
+                Ray ray(position, direction, MAX_RECURSION);
+                image[i][j] = ray.trace();
+            }
+        }
+        
     }
 
 }
