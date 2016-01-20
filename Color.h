54708efdbd59bcf67c66acbedf3406357ff3f42e
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-20 14:31:45 +0100

    added operator+=(Color) to Color

--- Color.h
+++ Color.h
@@ -25,6 +25,7 @@ namespace raytracing {
     Color operator+(Color c2) { return Color(r+c2.R(),g+c2.G(),b+c2.B());}
     Color operator*(Color c2) { return Color(r*c2.R(),g*c2.G(),b*c2.B());}
     Color operator*(double d) { return Color(r*d,g*d,b*d);}
+    Color operator+=(Color c2) {return *this + this;}
 
 
     virtual void serialize(const string &indent = ""){
