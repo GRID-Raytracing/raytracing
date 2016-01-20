595acbe0fe56e37d5f8cd72d4c657a1609113291
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-20 14:10:33 +0100

    Fixing imports of Sphere.h

--- ShadowedSphere.cpp
+++ ShadowedSphere.cpp
@@ -8,7 +8,7 @@ Color ShadowedSphere::getColorAtIntersection(Vector3D i, Ray r) {
 	
 	Color diffuse = Color();
 	
-	for(light : lights){
+	for(LightSource* light : lights){
 		if !isShadowed(Ray(i,(light->getPosition()-i).normalize()), light){
 			double cosAngle = (i-position).normalise()*(light->getPosition()-i).normalise();
 			cosAngle = (cosAngle>0)?cosAngle:0;
