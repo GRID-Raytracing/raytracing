85319cec05b2e462c32f89c6643cc45a81606a11
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-20 14:29:45 +0100

    fixed errors

--- Ray.h
+++ Ray.h
@@ -15,7 +15,7 @@ namespace raytracing {
 		unsigned int step;
 		
 	public:
-		Ray(Vector3D& o, Vector3D& d, unsigned int s): origin(o), direction(d), step(s) {};
+		Ray(Vector3D o, Vector3D d, unsigned int s): origin(o), direction(d), step(s) {};
 		Color trace();
 		Vector3D getOrigin() { return origin;};
 		Vector3D getDirection() { return direction;};
