13d8c3d2e3e77a4e74b7faa796a516777e90dd1e
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-20 15:04:37 +0100

    added constructors to ShadowedSphere and PointLightSource

--- ShadowedSphere.h
+++ ShadowedSphere.h
@@ -12,7 +12,7 @@ namespace raytracing {
 	class ShadowedSphere : public Sphere {
 	
 	public:
-
+		ShadowedSphere(Vector3D pos, double ref, Color col, double t, double r) : Sphere(pos, ref, col, t, r) {};
 		virtual Color getColorAtIntersection(Vector3D i, Ray r) override;
 		virtual bool isShadowed(Ray r, LightSource* light);
 	};
