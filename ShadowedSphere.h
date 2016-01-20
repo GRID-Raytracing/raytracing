c2a64543c919466071d83c721a3283de513cb73f
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-20 13:59:13 +0100

    fixed imports of ShadowedSphere.h

--- ShadowedSphere.h
+++ ShadowedSphere.h
@@ -1,6 +1,11 @@
 #ifndef SHADOWEDSPHERE_H
 #define SHADOWEDSPHERE_H
 
+#include "Sphere.h"
+#include "Color.h"
+#include "Vector3D.h"
+#include "Ray.h"
+
 namespace raytracing {
 
 	class ShadowedSphere : public Sphere {
