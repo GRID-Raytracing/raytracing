b5599840e9cbde8e17b454edf14aaa3dd0c1145d
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-20 14:18:15 +0100

    added signature of isShadowed to header

--- ShadowedSphere.h
+++ ShadowedSphere.h
@@ -5,6 +5,7 @@
 #include "Color.h"
 #include "Vector3D.h"
 #include "Ray.h"
+#include "LightSource.h"
 
 namespace raytracing {
 
@@ -13,6 +14,7 @@ namespace raytracing {
 	public:
 
 		virtual Color getColorAtIntersection(Vector3D i, Ray r) override;
+		virtual bool isShadowed(Ray r, LightSource* light);
 	};
 
 }
