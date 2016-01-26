97a98f089fbd683c771b0cc40f19c585891d9931
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-26 15:46:26 +0100

    fixed constructor for spherical light source

--- SphericalLightSource.h
+++ SphericalLightSource.h
@@ -2,6 +2,8 @@ namespace raytracing {
 	
 	class SphericalLightSource: public LightSource, public Sphere {
 	public:
+		SphericalLightSource(Vector3D pos, double ref, Color col, double t, double r) :
+    		Sphere(pos, ref, col, t, r) {}
 		virtual Color getColorAtIntersection( Vector3D i, Ray r) override {return color+getReflectionAtIntersection(i,r);};
 	}
 
