a34db629b2ec236b7b8208572a4d6653c773df89
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-26 15:37:05 +0100

    fix

--- tests/SphereField.cpp
+++ tests/SphereField.cpp
@@ -15,8 +15,8 @@
  int main(){
 	 Scene* scene = Scene::getInstance();
 	 
-	 for(i=0; i<10; i++) {
-	 	for(j=0; j<10; j++) {
+	 for(int i=0; i<10; i++) {
+	 	for(int j=0; j<10; j++) {
 	 		scene->addDrawableObject(new Sphere(Vector3D(-5+i,-5+j,10),0.8, Color(rand(),rand(),rand()), 0.0, 1.0);
 	 	}
 	 }
