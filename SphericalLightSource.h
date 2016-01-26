219d848541c0969d2334ba4669607fad7939220b
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-26 16:05:31 +0100

    fix

--- SphericalLightSource.h
+++ SphericalLightSource.h
@@ -5,7 +5,7 @@ namespace raytracing {
 		SphericalLightSource(Vector3D pos, double ref, Color col, double t, double r) :
     		Sphere(pos, ref, col, t, r), LightSource(pos,ref,col,t) {}
 		virtual Color getColorAtIntersection( Vector3D i, Ray r) override {
-			return (color+getReflectionAtIntersection(i,r));
+			return (LightSource::color+Sphere::getReflectionAtIntersection(i,r));
 		};
 	};
 
