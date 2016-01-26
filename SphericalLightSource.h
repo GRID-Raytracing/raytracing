5659ff1af0dd8f0572dc1598998a374b5236dadb
Jens Jäschke <1462752@uni-wuppertal.de>
2016-01-26 15:00:26 +0100

    implemented SphericalLightSource

--- SphericalLightSource.h
+++ SphericalLightSource.h
@@ -1,7 +1,8 @@
 namespace raytracing {
 	
 	class SphericalLightSource: public LightSource, public Sphere {
-	
+	public:
+		virtual Color getColorAtIntersection( Vector3D i, Ray r) override {return color+getReflectionAtIntersection(i,r);};
 	}
 
 }
