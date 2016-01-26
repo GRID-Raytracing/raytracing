edee9ef8d9a6e128ce55d1fb3ded96cbfae629a6
Mana Sasagawa <mn.9a1.ssgw@gmail.com>
2016-01-26 14:23:30 +0100

    recommit

--- cuboid.h
+++ cuboid.h
@@ -1,31 +1,33 @@
 #ifndef CUBIOD_H
 #define CUBIOD_H
 
+using namespace std;
+
 namespace raytracing{
-class Cuboid: public drawableObject
-{
-private:
-	double xSize;
-	double ySize;
-	double zSize;
-public:
-	//constructor
-	Cuboid(){}
-	Cuboid(double ref, Color col, double t, double x, double y, double z): drawableObject(ref,col,t),xSize(x), ySize(y), zSize(z) {}	
-
-	//setter
-	void setxSize(double x):xSize(x){};
-	void setySize(double y):ySize(y){};
-	void setzSize(double z):zSize(z){};
-	
-	//getter
-	void getxSize() {return x;}
-	void getySize() {return y;}
-	void getzSize() {return z;}
-
-	
-	Vector3D intersection (Ray r);
-	Color getColorAtIntersection( Vector3D intersection, Ray r);
-};
+    class Cuboid: public drawableObject{
+        
+    private:
+        double xSize;
+        double ySize;
+        double zSize;
+    public:
+        //constructor
+        Cuboid(Vector3D pos, double ref, Color col, double t, double x, double y, double z):
+        drawableObject(pos, ref, col, t), xSize(x), ySize(y), zSize(z) {}
+        
+        //setter
+        void setxSize(double x){xSize=x;}
+        void setySize(double y){ySize=y;}
+        void setzSize(double z){zSize=z;}
+        
+        //getter
+        double getxSize() {return xSize;}
+        double getySize() {return ySize;}
+        double getzSize() {return zSize;}
+        
+        
+        virtual double intersection (Ray r) override;
+        virtual Color getColorAtIntersection( Vector3D intersection, Ray r) override;
+    };
 }
 #endif // CUBIOD_H
