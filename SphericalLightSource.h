460238f98773f706eea3ce622562fa8bf61b63c8
Jens Jäschke <jensm1@bbjh.de>
2016-01-29 15:29:00 +0100

    Fixed imports in SphericalLightSource

--- SphericalLightSource.h
+++ SphericalLightSource.h
@@ -1,11 +1,11 @@
 #ifndef SPHERICALLIGHTSOURCE_H
 #define SPHERICALLIGHTSOURCE_H
 
-#include LightSource.h
-#include Sphere.h
-#include Color.h
-#include Vector3D.h
-#include Ray.h
+#include "LightSource.h"
+#include "Sphere.h"
+#include "Color.h"
+#include "Vector3D.h"
+#include "Ray.h"
 
 namespace raytracing {
 
